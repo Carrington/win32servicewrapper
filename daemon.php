6672cba1d61745fa1201f4cd7c15b7df99c6120e
Daniel Ingraham <daniel@danielingraham.com>
2015-11-26 10:20:54 -0500

    retab

--- daemon.php
+++ daemon.php
@@ -4,18 +4,18 @@ define('WIN32_ERROR_CALL_NOT_IMPLEMENTED', 120);
 
 //Base class for Win32 services
 abstract class Daemon {
-	protected $serviceName					= '';
-	protected $serviceDisplayName	  = '';
-	protected $serviceDescription		= '';
-	protected $user									= null;
-	protected $password							= null;
-	protected $path									= 'C:\\wamp\\bin\\php\\php5.5.14\\php-win.exe';
-	protected $svc_type							= WIN32_SERVICE_WIN32_OWN_PROCESS;
-	protected $start_type						= WIN32_SERVICE_AUTO_START;
-	protected $error_control				= WIN32_SERVICE_ERROR_IGNORE;
-	protected $delayed_start				= false;
-	protected $base_priority				= WIN32_NORMAL_PRIORITY_CLASS;
-	protected $msgs									= array();
+	protected $serviceName		= '';
+	protected $serviceDisplayName	= '';
+	protected $serviceDescription	= '';
+	protected $user			= null;
+	protected $password		= null;
+	protected $path			= 'C:\\wamp\\bin\\php\\php5.5.14\\php-win.exe';
+	protected $svc_type		= WIN32_SERVICE_WIN32_OWN_PROCESS;
+	protected $start_type		= WIN32_SERVICE_AUTO_START;
+	protected $error_control	= WIN32_SERVICE_ERROR_IGNORE;
+	protected $delayed_start	= false;
+	protected $base_priority	= WIN32_NORMAL_PRIORITY_CLASS;
+	protected $msgs			= array();
 	
 	//Pre-run work
 	abstract protected function setupRun($params = null);
@@ -34,18 +34,18 @@ abstract class Daemon {
 	
 	public function create() {
 		$params =  array(
-			"service" 		=> $this->serviceName,
-			"dispaly" 		=> $this->serviceDisplayName,
+			"service" 	=> $this->serviceName,
+			"dispaly" 	=> $this->serviceDisplayName,
 			"description" 	=> $this->serviceDescription,
-			"path"			=> $this->path,
-			"svc_type"		=> $this->svc_type,
+			"path"		=> $this->path,
+			"svc_type"	=> $this->svc_type,
 			"start_type"	=> $this->start_type,
 			"error_control"	=> $this->error_control,
 			"delayed_start"	=> $this->delayed_start,
 			"base_priority" => $this->base_priority
 		);
 		if ($this->user) {
-			$params['user'] = $this->user;
+			$params['user']     = $this->user;
 			$params['password'] = $this->password;
 		}
 		if(win32_create_service($params) == WIN32_NO_ERROR) {
